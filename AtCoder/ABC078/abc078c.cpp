a34de534a65b1d22e0ed7d4a586fa59895b509ee
kz0420 <49297531+kz0420@users.noreply.github.com>
2019-04-15 16:15:51 +0900

    Create abc078c.cpp

---
+++ AtCoder/ABC078/abc078c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <set>
+#include <algorithm>
+#include <array>
+#include <complex>
+#include <string>
+#include <utility>
+#include <map>
+#include <queue>
+#include <list>
+#include <functional>
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+const int INF = 1 << 21;
+using pii = std::pair<int, int>;
+#define SORT(v) std::sort(v.begin(), v.end())
+int main(void) {
+	int N, M;
+	int ans = 0;
+	std::cin >> N >> M;
+	ans = 1900 * M + (N - M) * 100;
+	ans *= std::pow(2, M);
+	std::cout << ans << std::endl;
+	return 0;
+}
